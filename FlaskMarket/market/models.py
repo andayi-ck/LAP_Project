dec9c0bd126e3e0c827081bc361204c24a10b8b0
Morgan N. Andayi <143075077+andayi-ck@users.noreply.github.com>
2025-05-31 09:10:38 +0300

    Update models.py

--- FlaskMarket/market/models.py
+++ FlaskMarket/market/models.py
@@ -41,4 +41,50 @@ class Item(db.Model):
     def can_sell(self, item_obj):
         return item_obj in self.items
 
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(length=30), nullable=False, unique=True)
+    email_address = db.Column(db.String(length=50), nullable=False, unique=True)
+    password_hash = db.Column(db.String(length=60), nullable=False)
+    budget = db.Column(db.Integer(), nullable=False, default=1000)
+    items = db.relationship('Item', backref='owned_user', lazy=True)
+    email_verified = db.Column(db.Boolean, default=False)
+    role = db.Column(db.String(20), nullable=False, default='normal_user')
+    
+    def __repr__(self):
+        return f'<User {self.username}>'
+    
+    
+    def get_id(self):
+        
+    
+        return str(self.id)
+    
+    
+    @property
+    def prettier_budget(self):
+        if len(str(self.budget)) >= 4:
+            return f'{str(self.budget)[:-3]},{str(self.budget)[-3:]}$'
+        else:
+            return f"{self.budget}$"
+
+    @property
+    def password(self):
+        return self.password
+    
+    @password.setter
+    def password(self, plain_text_password):
+        #self.password_hash = plain_text_password
+        self.password_hash = bcrypt.generate_password_hash(plain_text_password).decode('utf-8')
+        
+    def check_password_correction(self, attempted_password):
+        return bcrypt.check_password_hash(self.password_hash, attempted_password)
+
+    def can_purchase(self, item_obj):
+        return self.budget >= item_obj.price
+    
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
  
